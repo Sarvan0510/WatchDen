5cbad650170e1d7ce4b99af9005449fbd24c092a
Abhishek Singh <abhishek2511.25@gmail.com>
2026-01-24 12:47:51 +0530

    Added services

---
+++ backend/user-service/src/test/java/com/watchden/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchden.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
