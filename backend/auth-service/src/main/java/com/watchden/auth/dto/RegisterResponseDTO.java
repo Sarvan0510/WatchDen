0464df376ab0b4a309b7366a75a0a8f25acfcfe2
Abhishek Singh <abhishek2511.25@gmail.com>
2026-01-26 00:36:51 +0530

    Adding JWT with Validations

---
+++ backend/auth-service/src/main/java/com/watchden/auth/dto/RegisterResponseDTO.java
@@ -0,0 +1,20 @@
+package com.watchden.auth.dto;
+
+public class RegisterResponseDTO {
+	
+	private String message;
+
+	public RegisterResponseDTO(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
--- backend/auth-service/src/main/java/com/watchden/auth/dto/UsersDTO.java
+++ backend/auth-service/src/main/java/com/watchden/auth/dto/UsersDTO.java
@@ -1,13 +1,41 @@
 package com.watchden.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class UsersDTO {
 	
+	@NotBlank(message = "Username cannot be blank")
+	@Size(min = 4, max = 20, message = "Username must be 4–20 characters")
+	@Pattern(
+		regexp = "^[a-zA-Z0-9._]+$",
+		message = "Username can contain letters, numbers, dot and underscore only"
+	)
 	private String username;
+	
+	@NotBlank(message = "Email cannot be blank")
+	@Email(message = "Invalid email format")
+	@Size(max = 100, message = "Email must not exceed 100 characters")
     private String email;
+    
+	@NotBlank(message = "Password cannot be blank")
+	@Size(min = 8, max = 64, message = "Password must be 8–64 characters")
+	@Pattern(
+		regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).+$",
+		message = "Password must contain uppercase, lowercase, number, and special character"
+	)
     private String password;
-  
-    public String getUsername() {
+    
+    public UsersDTO(String username, String email, String password) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getUsername() {
         return username;
     }
  
